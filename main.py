49f7ed3cd9203e233bb531f01546271d93b21a4c
ThatOneCoolishGuy <elitaylor120@gmail.com>
2026-03-05 18:28:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
 import requests
 print("Welcome to BananaGrab!")
-time.sleep(1)
-t = input("Please enter the URL of the website to fetch. - ")
-time.sleep(1)
-r = requests.get(t)
-print(r.text)
+while True:
+  time.sleep(1)
+  t = input("Please enter the URL of the website to fetch. - ")
+  time.sleep(1)
+  r = requests.get(t)
+  print(r.text)
